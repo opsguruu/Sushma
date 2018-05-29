ece782765b4f4e71f5bfaff7f952c01e755b3eff
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2018-05-29 15:33:04 +0500

    Replace/Remove HTML Elements

---
+++ replace-html-elements.php
@@ -0,0 +1,29 @@
+<?php
+/** Let\'s suppose $content contains the entire HTML or a part of the HTML
+ *  from which we need to replace/remove specific parts
+ */
+
+// Remove the element which has the element2 ID
+$content = preg_replace(
+  '#<div[^>]*id="element2"[^>]*>.*?</div>#is', '', $content
+);
+
+// Remove the element which has the yasglobal-translation ID
+$content = preg_replace(
+  '#<table[^>]*id="yasglobal-translation"[^>]*>.*?</table>#is', '', $content
+);
+
+// Remove the elements which have the wrapper class
+$content = preg_replace(
+  '#<table[^>]*class="wrapper"[^>]*>.*?</table>#is', '', $content
+);
+
+// Replace the element which has the element2 ID
+$content = preg_replace(
+  '#<div[^>]*id="element2"[^>]*>.*?</div>#is', '<div>Testing Element</div>', $content
+);
+
+// Replace the element which has the yasglobal-translation ID
+$content = preg_replace(
+  '#<table[^>]*id="yasglobal-translation"[^>]*>.*?</table>#is', '<div>Testing Element</div>', $content
+);
--- validate-url.php
+++ validate-url.php
@@ -1,16 +1,13 @@
 <?php
-
 /**
- * `FILTER_VALIDATE_URL` is used to filter the url. It gives you false if the provided field is not an URL.
+ * `FILTER_VALIDATE_URL` is used to filter the url. It gives you false
+ * if the provided field is not an URL.
  * It is helpful with users form to verify the details. 
  */
 
-$check_url = "Hello World"; 
-
-if(filter_var($check_url, FILTER_VALIDATE_URL)) {
+$check_url = "Hello World";
+if (filter_var($check_url, FILTER_VALIDATE_URL)) {
   echo 'URL is valid, Please proceed';
 } else {
   echo 'Please provide the URL.';
 }
-
-?>
