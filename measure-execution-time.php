535e9bcfa9ef02e31f147c6996987b78def65a15
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2018-12-07 19:54:19 +0500

    Measure Execution Time

---
+++ measure-execution-time.php
@@ -0,0 +1,9 @@
+<?php
+$start = microtime(true);
+
+// WRITE YOUR CODE HERE OR CALL THE FUNCTION
+
+$time_elapsed_secs = microtime(true) - $start;
+
+//THIS IS THE TIME WHICH HAS BEEN TAKEN BY YOUR CODE
+print $time_elapsed_secs;
